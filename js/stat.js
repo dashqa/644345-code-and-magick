0a6c165538e3ac062d3448762894093e6967c1e6
Зайцева Дарья <errorka792@gmail.com>
2018-09-03 13:48:32 +0300

    done module2-task1

--- js/stat.js
+++ js/stat.js
@@ -5,10 +5,12 @@ var CLOUD_HEIGHT = 270;
 var CLOUD_X = 100;
 var CLOUD_Y = 10;
 var CLOUD_GAP = 10;
-var GAP = 25;
+var GAP = 15;
+var TEXT_GAP = 30;
 var BAR_GAP = 50;
 var BAR_WIDTH = 40;
-var BAR_HEIGHT = 150;
+var MAX_BAR_HEIGHT = 150;
+var NAME_Y = CLOUD_HEIGHT - GAP;
 
 var getRandomInt = function (min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
@@ -36,22 +38,25 @@ window.renderStatistics = function (ctx, players, times) {
 
   ctx.font = '16px PT Mono';
   ctx.textBaseline = 'hanging';
-  ctx.fillText('Ура вы победили!', CLOUD_X + GAP, GAP);
-  ctx.fillText('Список результатов:', CLOUD_X + GAP, GAP * 1.5);
+  ctx.fillText('Ура вы победили!', CLOUD_X + GAP, TEXT_GAP);
+  ctx.fillText('Список результатов:', CLOUD_X + GAP, TEXT_GAP * 1.5);
 
 
   var maxTime = getMaxElement(times);
   for (var i = 0; i < players.length; i++) {
-    var positionX = CLOUD_X + GAP + (BAR_GAP + BAR_WIDTH) * i; // координаты по Х
+    var positionX = CLOUD_X + BAR_GAP + (BAR_GAP + BAR_WIDTH) * i; // координаты по Х
+    var BAR_HEIGHT = (MAX_BAR_HEIGHT * times[i]) / maxTime; // высота столба с учетом результата
+    var BAR_POSITION_Y = NAME_Y - GAP - BAR_HEIGHT; // координата столбца по Y
 
-    ctx.fillText(Math.round(times[i]), positionX, (BAR_HEIGHT * times[i]) / maxTime + 65);
-    ctx.fillText(players[i], positionX, CLOUD_HEIGHT - GAP);
+    ctx.fillStyle = '#000';
+    ctx.fillText(Math.round(times[i]), positionX, BAR_POSITION_Y - GAP);
+    ctx.fillText(players[i], positionX, NAME_Y);
 
     if (players[i] === 'Вы') {
       ctx.fillStyle = 'rgba(255, 0, 0, 1)';
     } else {
       ctx.fillStyle = 'rgb(0, 0, ' + getRandomInt(1, 256) + ')';
     }
-    ctx.fillRect(positionX, CLOUD_HEIGHT - GAP - BAR_HEIGHT - GAP * 1.5, BAR_WIDTH, (BAR_HEIGHT * times[i]) / maxTime);
+    ctx.fillRect(positionX, BAR_POSITION_Y, BAR_WIDTH, BAR_HEIGHT);
   }
 };
