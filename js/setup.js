4849d161a9ddfe0c602d8859b66fc3701082155c
dashqa <errorka792@gmail.com>
2018-09-07 01:40:04 +0300

    for Trevis

--- js/setup.js
+++ js/setup.js
@@ -1,10 +1,10 @@
 'use strict';
 
 var WIZARDS_AMOUNT = 4;
-var WIZARD_NAME = ['Иван','Хуан Себастьян','Мария','Кристоф','Виктор','Юлия','Люпита','Вашингтон'];
-var WIZARD_SURNAME = ['да Марья','Верон','Мирабелла','Вальц','Онопко','Топольницкая','Нионго','Ирвинг'];
-var COAT_COLOR = ['rgb(101, 137, 164)','rgb(241, 43, 107)','rgb(146, 100, 161)','rgb(56, 159, 117)','rgb(215, 210, 55)','rgb(0, 0, 0)'];
-var EYES_COLOR = ['red','black','green','yellow','purle','blue'];
+var WIZARD_NAME = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+var WIZARD_SURNAME = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+var COAT_COLOR = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+var EYES_COLOR = ['red', 'black', 'green', 'yellow', 'purle', 'blue'];
 
 var setupBlock = document.querySelector('.setup');
 setupBlock.classList.remove('hidden');
@@ -15,15 +15,15 @@ var similarWizardTemplate = document.querySelector('#similar-wizard-template')
     .querySelector('.setup-similar-item'); // элемент, который будем копировать
 
 
-var getRandomInd = function(array) {
+var getRandomInd = function (array) {
  return Math.floor(Math.random() * array.length);
 };
 
-var getWizardName = function(name, surname) {
+var getWizardName = function (name, surname) {
     return name[getRandomInd(name)] + ' ' + surname[getRandomInd(surname)];
 };
 
-var getColor = function(color) {
+var getColor = function (color) {
   return color[getRandomInd(color)];
 };
 
