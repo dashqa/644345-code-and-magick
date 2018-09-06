e0155c2eaa007a888cfb47cc71994542b4b96d86
dashqa <errorka792@gmail.com>
2018-09-07 01:43:05 +0300

    trevis

--- js/setup.js
+++ js/setup.js
@@ -11,23 +11,23 @@ setupBlock.classList.remove('hidden');
 
 var similarListElement = document.querySelector('.setup-similar-list'); // родитель, в который будем вставлять
 var similarWizardTemplate = document.querySelector('#similar-wizard-template')
-    .content
-    .querySelector('.setup-similar-item'); // элемент, который будем копировать
+  .content
+  .querySelector('.setup-similar-item'); // элемент, который будем копировать
 
 
 var getRandomInd = function (array) {
- return Math.floor(Math.random() * array.length);
+  return Math.floor(Math.random() * array.length);
 };
 
 var getWizardName = function (name, surname) {
-    return name[getRandomInd(name)] + ' ' + surname[getRandomInd(surname)];
+  return name[getRandomInd(name)] + ' ' + surname[getRandomInd(surname)];
 };
 
 var getColor = function (color) {
   return color[getRandomInd(color)];
 };
 
-var getWizard = function() {
+var getWizard = function () {
   var wizard = [];
   return wizard = {
     name: getWizardName(WIZARD_NAME, WIZARD_SURNAME),
@@ -37,19 +37,17 @@ var getWizard = function() {
 };
 
 var renderWizard = function (wizard) {
-  var wizardElement = similarWizardTemplate.cloneNode(true); //клонируем темплейтный элемент
+  var wizardElement = similarWizardTemplate.cloneNode(true); // клонируем темплейтный элемент
   wizardElement.querySelector('.setup-similar-label').textContent = wizard.name; // задаем текст лейбла
-  wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor; //задаем цвет плаща
-  wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor; //задаем цвет глаз
+  wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor; // задаем цвет плаща
+  wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor; // задаем цвет глаз
   return wizardElement;
-}
+};
 
 var fragment = document.createDocumentFragment(); // создаем корзину, которая соберем всех новых магов
 for (var i = 0; i < WIZARDS_AMOUNT; i++) {
-  fragment.appendChild(renderWizard(getWizard())); //в корзину добавляем магов по одному
-}
+  fragment.appendChild(renderWizard(getWizard())); // в корзину добавляем магов по одному
+};
 similarListElement.appendChild(fragment); // вставляем разом всех магов из корзины
 
 setupBlock.querySelector('.setup-similar').classList.remove('hidden');
-
-
