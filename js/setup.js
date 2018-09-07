68e234769dfa247725450ff84fa2acbf226d2c13
Зайцева Дарья <errorka792@gmail.com>
2018-09-07 13:42:56 +0300

    add array createWizards

--- js/setup.js
+++ js/setup.js
@@ -1,10 +1,12 @@
 'use strict';
 
 var WIZARDS_AMOUNT = 4;
-var WIZARD_NAME = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
-var WIZARD_SURNAME = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
-var COAT_COLOR = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
-var EYES_COLOR = ['red', 'black', 'green', 'yellow', 'purle', 'blue'];
+var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+var EYES_COLORS = ['red', 'black', 'green', 'yellow', 'purle', 'blue'];
+var HEAD_COLORS = ['#015500', '#ba76f4', '#0a5191', '#17525b', '#c08a80', '#a12229', '#32b4b6', '#003908', '#e71f61'];
+var HANDS_COLORS = ['#381100', '#3269ad', '#852fb0', '#69bf3e', '#683765', '#6ea272', '#f8f60c', '#0306e9', '#3f11ab', '#c4f257'];
 
 var setupBlock = document.querySelector('.setup');
 setupBlock.classList.remove('hidden');
@@ -27,27 +29,39 @@ var getColor = function (color) {
   return color[getRandomInd(color)];
 };
 
-var getWizard = function () {
-  var wizard = [];
-  return wizard = {
-    name: getWizardName(WIZARD_NAME, WIZARD_SURNAME),
-    coatColor: getColor(COAT_COLOR),
-    eyesColor: getColor(EYES_COLOR)
+var getWizardParam = function () {
+  return {
+    name: getWizardName(WIZARD_NAMES, WIZARD_SURNAMES),
+    coatColor: getColor(COAT_COLORS),
+    eyesColor: getColor(EYES_COLORS),
+    headColor: getColor(HEAD_COLORS),
+    handsColor: getColor(HANDS_COLORS)
   };
 };
 
+var createWizards = function (amount) {
+  var wizardsArray = [];
+  for (var i = 0; i < amount; i++) {
+    wizardsArray.push(getWizardParam());
+  }
+  return wizardsArray;
+};
+
+
 var renderWizard = function (wizard) {
-  var wizardElement = similarWizardTemplate.cloneNode(true); // клонируем темплейтный элемент
-  wizardElement.querySelector('.setup-similar-label').textContent = wizard.name; // задаем текст лейбла
-  wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor; // задаем цвет плаща
-  wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor; // задаем цвет глаз
+  var wizardElement = similarWizardTemplate.cloneNode(true);
+  wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
+  wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
+  wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
+  wizardElement.querySelector('.wizard-head').style.fill = wizard.headColor;
+  wizardElement.querySelector('.wizard-hands').style.fill = wizard.handsColor;
   return wizardElement;
 };
 
 var fragment = document.createDocumentFragment(); // создаем корзину, которая соберем всех новых магов
-for (var i = 0; i < WIZARDS_AMOUNT; i++) {
-  fragment.appendChild(renderWizard(getWizard())); // в корзину добавляем магов по одному
-};
+for (var i = 0; i < createWizards(WIZARDS_AMOUNT).length; i++) {
+  fragment.appendChild(renderWizard(createWizards(WIZARDS_AMOUNT)[i])); // в корзину добавляем магов по одному
+}
 similarListElement.appendChild(fragment); // вставляем разом всех магов из корзины
 
 setupBlock.querySelector('.setup-similar').classList.remove('hidden');
